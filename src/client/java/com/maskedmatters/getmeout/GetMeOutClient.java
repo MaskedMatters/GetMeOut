c50c892d3152151faa52928d4a9174d064956e03
Matthew West <masked.matters@gmail.com>
2025-08-05 19:39:18 -0400

    Refactor 1.21.5-1.20.5 for Java

--- src/client/java/com/maskedmatters/getmeout/GetMeOutClient.java
+++ src/client/java/com/maskedmatters/getmeout/GetMeOutClient.java
@@ -7,6 +7,7 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.toast.SystemToast;
 import net.minecraft.client.util.InputUtil;
 
 import net.minecraft.text.Text;
@@ -28,9 +29,14 @@ public class GetMeOutClient implements ClientModInitializer {
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			if (quitKeybind.wasPressed() && client.world != null) {
-				client.world.disconnect(Text.literal("Disconnected!"));
-				client.disconnectWithSavingScreen();
-				client.setScreen(new TitleScreen());
+				client.world.disconnect();
+				client.getToastManager().add(
+						SystemToast.create(client, SystemToast.Type.NARRATOR_TOGGLE, Text.of("Saving Game"), Text.of("Don't close Minecraft until text closes."))
+				);
+				client.disconnect(new TitleScreen());
+				client.getToastManager().add(
+						SystemToast.create(client, SystemToast.Type.NARRATOR_TOGGLE, Text.of("Game Saved"), Text.of("You can safely close Minecraft."))
+				);
 			}
 		});
 	}
