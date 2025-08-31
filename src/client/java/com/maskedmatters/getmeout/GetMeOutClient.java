e656323244ffd9696dc2ae363012df51d00c7a45
Matthew West <masked.matters@gmail.com>
2025-08-30 21:41:30 -0400

    Add support for 1.20.2-1.20.4 and default key

--- src/client/java/com/maskedmatters/getmeout/GetMeOutClient.java
+++ src/client/java/com/maskedmatters/getmeout/GetMeOutClient.java
@@ -7,6 +7,7 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.toast.SystemToast;
 import net.minecraft.client.util.InputUtil;
 
 import net.minecraft.text.Text;
@@ -21,16 +22,21 @@ public class GetMeOutClient implements ClientModInitializer {
 				new KeyBinding(
 						"key.getmeout.quit",
 						InputUtil.Type.KEYSYM,
-						GLFW.GLFW_KEY_UNKNOWN,
+						GLFW.GLFW_KEY_F9,
 						"category.getmeout"
 				)
 		);
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			if (quitKeybind.wasPressed() && client.world != null) {
-				client.world.disconnect(Text.literal("Disconnected!"));
-				client.disconnectWithSavingScreen();
-				client.setScreen(new TitleScreen());
+				client.world.disconnect();
+				client.getToastManager().add(
+						SystemToast.create(client, SystemToast.Type.NARRATOR_TOGGLE, Text.of("Saving Game"), Text.of("Don't close Minecraft until text closes."))
+				);
+				client.disconnect(new TitleScreen());
+				client.getToastManager().add(
+						SystemToast.create(client, SystemToast.Type.NARRATOR_TOGGLE, Text.of("Game Saved"), Text.of("You can safely close Minecraft."))
+				);
 			}
 		});
 	}
