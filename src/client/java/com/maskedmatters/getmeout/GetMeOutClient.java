5da3245e8d6bef6e5242b107a8d9ecd5e2177067
Matthew <masked.matters@gmail.com>
2026-03-27 16:33:27 -0400

    chore: update to 26.1, official Mojang mappings

--- src/client/java/com/maskedmatters/getmeout/GetMeOutClient.java
+++ src/client/java/com/maskedmatters/getmeout/GetMeOutClient.java
@@ -1,39 +1,36 @@
 package com.maskedmatters.getmeout;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
-
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-
-import net.minecraft.client.gui.screen.TitleScreen;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-
-import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.resources.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 public class GetMeOutClient implements ClientModInitializer {
+
+	private static KeyMapping quitKeybind;
+	private static final KeyMapping.Category GETMEOUT_CATEGORY = KeyMapping.Category.register(Identifier.fromNamespaceAndPath("getmeout", "main"));
+
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 
-        KeyBinding.Category GETMEOUT_CATEGORY = KeyBinding.Category.create(Identifier.of("getmeout"));
-
-		KeyBinding quitKeybind = KeyBindingHelper.registerKeyBinding(
-				new KeyBinding(
+		quitKeybind = KeyMappingHelper.registerKeyMapping(
+				new KeyMapping(
 						"key.getmeout.quit",
-						InputUtil.Type.KEYSYM,
-						GLFW.GLFW_KEY_F9,
+						InputConstants.Type.KEYSYM,
+						GLFW.GLFW_KEY_PERIOD,
 						GETMEOUT_CATEGORY
 				)
 		);
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			if (quitKeybind.wasPressed() && client.world != null) {
-				client.world.disconnect(Text.literal("Disconnected!"));
-				client.disconnectWithSavingScreen();
-				client.setScreen(new TitleScreen());
+			while (quitKeybind.consumeClick()) {
+				if (client.getConnection() != null) {
+					client.disconnect(new TitleScreen(), false);
+				}
 			}
 		});
 	}
